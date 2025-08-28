c8de5b8767137f695dc25dd6caddd6d83bb6baf9
Cassiano <saurinho.64.ss@gmail.com>
2025-08-28 10:52:14 -0300

    First commit

---
+++ src/main/java/dev/cacassiano/review_analizr/ReviewAnalizrApplication.java
@@ -0,0 +1,13 @@
+package dev.cacassiano.review_analizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAnalizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAnalizrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cacassiano/review_analizr/ReviewAnalizrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cacassiano.review_analizr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAnalizrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
